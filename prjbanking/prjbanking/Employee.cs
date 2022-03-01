9aec01c0fc36f4b0a5891c8a989ab90ef5710a8a
nallalasagar <nallalasagar@gmail.com>
2022-03-01 17:25:21 +0530

    wedrcvg

---
+++ prjbanking/prjbanking/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjbanking
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public void GetEmployee()
+        {
+            Console.WriteLine("Enter the id");
+            id = Convert.ToInt32(Console.ReadLine());
+            name = Console.ReadLine();
+        }
+
+    }
+
+    
+}
